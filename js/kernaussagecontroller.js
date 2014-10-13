d864e4a35ce94ead22699d08645ec8f1c19d376f
Datokrat <pa.rei@gmx.de>
2014-10-13 19:29:31 +0200

    contextsynchronizer for kernaussage

--- js/kernaussagecontroller.js
+++ js/kernaussagecontroller.js
@@ -1,4 +1,4 @@
-define(["require", "exports", 'synchronizers/ksynchronizers', 'contentcontroller', 'contentviewmodel'], function(require, exports, KSync, ContentController, ContentViewModel) {
+define(["require", "exports", 'synchronizers/ksynchronizers', 'contentviewmodel'], function(require, exports, KSync, ContentViewModel) {
     var Controller = (function () {
         function Controller(model, viewModel, communicator) {
             this.init(model, viewModel, communicator);
@@ -6,15 +6,17 @@ define(["require", "exports", 'synchronizers/ksynchronizers', 'contentcontroller
         Controller.prototype.init = function (model, viewModel, communicator) {
             var _this = this;
             viewModel.isActive = ko.observable();
-            viewModel.general = ko.observable(new ContentViewModel.General);
-            viewModel.context = ko.observable(new ContentViewModel.Context);
             this.viewModel = viewModel;
 
+            viewModel.context = ko.observable(new ContentViewModel.Context);
+            this.contextSynchronizer = new KSync.ContextSynchronizer(communicator.content).setViewModelChangedHandler(function (context) {
+                return _this.viewModel.context(context);
+            }).setModelObservable(model.context);
+
+            viewModel.general = ko.observable(new ContentViewModel.General);
             this.generalContentSynchronizer = new KSync.GeneralContentSynchronizer(communicator.content).setViewModelChangedHandler(function (general) {
                 return _this.viewModel.general(general);
             }).setModelObservable(model.general);
-
-            this.context = new ContentController.Context(model.context(), viewModel.context(), communicator.content);
 
             viewModel.discussion = ko.observable();
             this.discussionSynchronizer = new KSync.DiscussionSynchronizer(communicator);
@@ -27,7 +29,7 @@ define(["require", "exports", 'synchronizers/ksynchronizers', 'contentcontroller
 
         Controller.prototype.dispose = function () {
             this.generalContentSynchronizer.dispose();
-            this.context.dispose();
+            this.contextSynchronizer.dispose();
             this.discussionSynchronizer.dispose();
         };
         return Controller;
--- js/kernaussagecontroller.ts
+++ js/kernaussagecontroller.ts
@@ -14,7 +14,7 @@ import ContentViewModel = require('contentviewmodel')
 export class Controller {
 	private viewModel: vm.ViewModel;
 	private generalContentSynchronizer: KSync.GeneralContentSynchronizer;
-	private context: ContentController.Context; //TODO: Synchronizer
+	private contextSynchronizer: KSync.ContextSynchronizer;
 	private discussionSynchronizer: KSync.DiscussionSynchronizer;
 
 	constructor(model: mdl.Model, viewModel: vm.ViewModel, communicator: com.Main) {
@@ -23,15 +23,17 @@ export class Controller {
 	
 	private init(model: mdl.Model, viewModel: vm.ViewModel, communicator: com.Main) {
 		viewModel.isActive = ko.observable<boolean>();
-		viewModel.general = ko.observable( new ContentViewModel.General );
-		viewModel.context = ko.observable( new ContentViewModel.Context );
 		this.viewModel = viewModel;
 		
+		viewModel.context = ko.observable( new ContentViewModel.Context );
+		this.contextSynchronizer = new KSync.ContextSynchronizer(communicator.content)
+			.setViewModelChangedHandler( context => this.viewModel.context(context) )
+			.setModelObservable(model.context);
+		
+		viewModel.general = ko.observable( new ContentViewModel.General );
 		this.generalContentSynchronizer = new KSync.GeneralContentSynchronizer(communicator.content)
 			.setViewModelChangedHandler( general => this.viewModel.general(general) )
 			.setModelObservable(model.general);
-
-		this.context = new ContentController.Context(model.context(), viewModel.context(), communicator.content);
 		
         viewModel.discussion = ko.observable<Discussable.ViewModel>();
 		this.discussionSynchronizer = new KSync.DiscussionSynchronizer(communicator);
@@ -48,7 +50,7 @@ export class Controller {
 	
 	public dispose() {
 		this.generalContentSynchronizer.dispose();
-		this.context.dispose();
+		this.contextSynchronizer.dispose();
 		this.discussionSynchronizer.dispose();
 	}
 }
