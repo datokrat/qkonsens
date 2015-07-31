ed6702490d20fd4c82336906ad1934a15d30bd80
Datokrat <pa.rei@gmx.de>
2015-07-31 15:35:25 +0200

    remove obsolete, superfluous code

--- js/index.js
+++ js/index.js
@@ -1,18 +1,12 @@
 ///<reference path="../typings/knockout.d.ts" />
 ///<reference path="../typings/jquery.d.ts" />
-define(["require", "exports", 'model', 'viewmodel', 'controller', 'konsenskistemodel', 'communicatorimpl'], function(require, exports, mdl, vm, ctr, koki, CommunicatorImpl) {
+define(["require", "exports", 'model', 'viewmodel', 'controller', 'communicatorimpl'], function(require, exports, mdl, vm, ctr, CommunicatorImpl) {
     
     infuser.defaults.templateUrl = "templates";
 
     var model = new mdl.ModelImpl();
-    var konsenskiste = new koki.Model;
-    konsenskiste.id(12);
-
-    //model.konsenskiste(konsenskiste);
     var viewModel = new vm.ViewModel();
     var communicator = new CommunicatorImpl;
-
-    //communicator.konsenskiste.query(12);
     var controller = new ctr.Controller(model, viewModel, communicator);
 
     ko.applyBindings(viewModel);
--- js/index.ts
+++ js/index.ts
@@ -8,18 +8,11 @@ import mdl = require('model')
 import vm = require('viewmodel')
 import ctr = require('controller')
 
-import koki = require('konsenskistemodel')
-import ka = require('kernaussagemodel')
-
 import CommunicatorImpl = require('communicatorimpl')
 
 var model = new mdl.ModelImpl();
-var konsenskiste = new koki.Model;
-konsenskiste.id(12);
-//model.konsenskiste(konsenskiste);
 var viewModel = new vm.ViewModel();
 var communicator = new CommunicatorImpl;
-//communicator.konsenskiste.query(12);
 var controller = new ctr.Controller(model, viewModel, communicator);
 
 ko.applyBindings(viewModel);
